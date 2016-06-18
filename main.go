5a476c8a569057f924fd4cfb1cad8815bd91c9c9
remeh <remeh@remeh.fr>
2016-06-18 19:54:15 +0200

    Remove the ES gamelist.xml write support.

--- main.go
+++ main.go
@@ -19,7 +19,6 @@ import (
 type Flags struct {
 	InputDirectory  string // in which directory to look for games
 	OutputDirectory string // in which directory outputing the resulting files (images) and gamelist.xml
-	Destination     string // Possible values: "emulationstation", "mehstation"
 	DestSqlite      string // If destination is mehstation, the mehstation database write in.
 	DestPlatform    int    // If destination is mehstation, the mehstation platform id to write for.
 	Extension       string // extension to look for, separated by a space
@@ -35,7 +34,6 @@ func ParseFlags() Flags {
 
 	flag.StringVar(&(flags.InputDirectory), "in-dir", "", "Input directories (directory containing games)")
 	flag.StringVar(&(flags.OutputDirectory), "out-dir", "", "Output directories for images and cover")
-	flag.StringVar(&(flags.Destination), "dest", "emulationstation", "Possible values: emulationstation, mehstation")
 	flag.StringVar(&(flags.DestSqlite), "meh-db", "database.db", "If destination is mehstation, the mehstation database write in.")
 	flag.IntVar(&(flags.DestPlatform), "meh-platform", -1, "If destination is mehstation, the mehstation database write in.")
 	flag.StringVar(&(flags.Extension), "ext", ".zip,.rar", "Accepted extensions")
@@ -46,10 +44,6 @@ func ParseFlags() Flags {
 
 	flag.Parse()
 
-	if flags.Destination != "emulationstation" && flags.Destination != "mehstation" {
-		log.Fatalf("Unsupported destination : %s\nPossible values: \"emulationstation\", \"mehstation\"\n", flags.Destination)
-	}
-
 	if len(flags.InputDirectory) > 0 && string(flags.InputDirectory[len(flags.InputDirectory)-1]) != "/" {
 		flags.InputDirectory = flags.InputDirectory + "/"
 	}
@@ -57,7 +51,7 @@ func ParseFlags() Flags {
 		flags.OutputDirectory = flags.OutputDirectory + "/"
 	}
 
-	if flags.Destination == "mehstation" && flags.DestPlatform == -1 {
+	if flags.DestPlatform == -1 {
 		log.Fatalf("To write in a mehstation DB, mehtadata needs the platform ID for which it will scrape metadata.")
 	}
 
@@ -125,7 +119,6 @@ func importFromEmulationStation(flags Flags) {
 }
 
 func main() {
-	var err error
 	flags := ParseFlags()
 
 	// Show platforms mode
@@ -160,21 +153,6 @@ func main() {
 	filenames := lookForFiles(flags.InputDirectory, exts)
 	gamesinfo := model.NewGamesinfo()
 
-	var file *os.File // For gamelist.xml for ES 2.0
-
-	// Create/lock the gamelist.xml for EmulationStation 2.0
-	// Do it now because it's useless to scrap everything if
-	// we can't write the result into gamelist.xml
-	if flags.Destination == "emulationstation" {
-		file, err = os.Create(flags.OutputDirectory + "gamelist.xml")
-
-		if err != nil {
-			log.Fatalf("[err] Can't create %sgamelist.xml: %s\n", flags.OutputDirectory, err.Error())
-		}
-	} else if flags.Destination == "mehstation" {
-
-	}
-
 	client := thegamesdb.NewClient()
 	for _, filename := range filenames {
 		gameinfo, err := client.Find(filename, platforms, flags.InputDirectory, flags.OutputDirectory, flags.MaxWidth)
@@ -194,19 +172,6 @@ func main() {
 		gamesinfo.AddGame(gameinfo)
 	}
 
-	if flags.Destination == "emulationstation" {
-		// builds the gamelist.xml
-		data, err := common.Encode(gamesinfo)
-		if err != nil {
-			log.Println("[err] Failed to encode the final xml:", err.Error())
-			os.Exit(1)
-		}
-
-		file.Write(data)
-		file.Close()
-
-		log.Println("gamelist.xml written.")
-	} else if flags.Destination == "mehstation" {
-		db.WriteDatabase(flags.DestSqlite, flags.DestPlatform, gamesinfo)
-	}
+	// writes executables info
+	db.WriteDatabase(flags.DestSqlite, flags.DestPlatform, gamesinfo)
 }
